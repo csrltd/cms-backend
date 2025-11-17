e92284e973f37bf82e14e249dc689e7444153491
umugisha one <umugishaone@gmail.com>
2025-11-17 15:47:30 +0200

    changing the secret_keymand Debug, but i've changed the ALLOWED_HOSTS!

--- core/settings.py
+++ core/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-crb3=#n+8@&j^=p2*r17qh=_7em_i^3qb^7^4n@b^w04$oj46g')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
